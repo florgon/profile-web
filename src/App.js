16ededffcbf4f4d12432c9fe382b379a121f2a48
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-04-10 15:16:45 +0400

    Movements, Cleanup, Bootstrap

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Libraries.
+import React from 'react';
+
+
+function App() {
+  // Core application.
+  return (
+    <div className="App">
+      Florgon profile.
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Libraries.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Styles.
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Rendering root.
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
