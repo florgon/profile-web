b80c27c6cd50efcbd9c07477a7e59df6728eb179
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-07-11 23:12:14 +0400

    Florgon VIP information modal window.

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import useAuth from '../contexts/auth';
 import Link from "next/link"
 import { useState } from 'react';
 import { authApiRequest } from '@kirillzhosul/florgon-auth-api';
-import {InputGroup, FormControl, Container, Card} from 'react-bootstrap'
+import {InputGroup, FormControl, Container, Card, Button, Modal} from 'react-bootstrap'
 
 const ConfirmationBanner = function({accessToken}){
 /// @description Banner component for not confirmed users.
@@ -51,7 +51,7 @@ const DeactivatedBanner = function(){
       </Card>);
 }
 
-const VIPBanner = function(){
+const VIPBanner = function({onReadMore, userIsVip}){
     return (
         <Card className="shadow-sm mt-3"  border="success">
           <Card.Body>
@@ -60,17 +60,23 @@ const VIPBanner = function(){
             </Card.Title>
             <Card.Text>
               <span className="mb-3 mt-3">
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-stars" viewBox="0 0 16 16">
-                        <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"/>
-                    </svg>
-                    {" "}
-                    You are Florgon VIP user! Enjoy your day! Thanks for your support!
-                    {" "}
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-stars" viewBox="0 0 16 16">
-                        <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"/>
-                    </svg>
-                <br/></span>
+                <div className="mb-1">
+                    {userIsVip && <>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-stars" viewBox="0 0 16 16">
+                            <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"/>
+                        </svg>
+                        {" "}
+                        You are Florgon VIP user! Enjoy your day! Thanks for your support!
+                        {" "}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-stars" viewBox="0 0 16 16">
+                            <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"/>
+                        </svg>
+                    </>}
+                    {!userIsVip && <>
+                        Get Florgon VIP status, to support Florgon, and get bonuses!
+                    </>}
+                </div>
+                <Button onClick={onReadMore}>Read more</Button>
               </span>
             </Card.Text>
           </Card.Body>
@@ -121,6 +127,7 @@ const calculateProfileFillness = function(user){
 
 export default function Profile() {
     const { user, loading, isAuthenticated, requestOauthAuthorization, accessToken } = useAuth();
+    const [vipModalOpened, setVipModalOpened] = useState(true);
     if (!isAuthenticated && !loading){
         requestOauthAuthorization()
         return (<div className="display-3 text-center"><b>Redirecting to authorization screen...</b></div>);
@@ -149,15 +156,14 @@ export default function Profile() {
                 <div>
             </div>
             </div>
+
             {!user.states?.is_confirmed && <ConfirmationBanner accessToken={accessToken}/>}
             {!user.states?.is_active && <DeactivatedBanner/>}
-            {user.states?.is_vip && <VIPBanner/>}
+            <VIPBanner onReadMore={() => setVipModalOpened(true)} userIsVip={user.states?.is_vip }/>
             {user.states?.is_admin && <AdminBanner/>}
-
 
             <div className="row mt-5 mb-5">
                 <div className="col-lg ml-lg-5 text-left">
-                    
                     <Container fluid className="w-75">
                         <InputGroup className="mb-2 shadow-sm">
                             <InputGroup.Text id="username-addon">Username</InputGroup.Text>
@@ -249,8 +255,6 @@ export default function Profile() {
                             />
                         </InputGroup>
 
-
-
                         <InputGroup className="mb-4 shadow-sm">
                             <InputGroup.Text id="registered-for-addon">Florgon member since</InputGroup.Text>
                             <FormControl
@@ -271,6 +275,61 @@ export default function Profile() {
                     </Container>
                 </div>
             </div>
+
+            <Modal show={vipModalOpened} onHide={() => setVipModalOpened(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-stars" viewBox="0 0 16 16">
+                        <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"/>
+                    </svg>
+                    {" "}
+                    Florgon VIP
+                    {" "}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-stars" viewBox="0 0 16 16">
+                        <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"/>
+                    </svg>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="input-group mb-1">
+                        <div className="input-group-prepend">
+                        <b>Florgon VIP</b>, is a status, that you may buy for special period of time (month, or year), if you are <b>Florgon VIP</b> user, you will receive special bonuses especially for you, and have increased limits in services.
+                            <hr/>
+                            Different services from <b>Florgon</b>, will give different bonuses for <b>Florgon VIP</b>. For example, <b>Gatey</b> will increase project count limitations (from 3 to 10), and other features.
+                            <hr/>
+                            <table className="table">
+                            <thead>
+                                <tr>
+                                <th scope="col">Service</th>
+                                <th scope="col">Bonus</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <th scope="row">Florgon</th>
+                                <td>Faster support. VIP user badge.</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">Florgon Gatey</th>
+                                <td>Increased project count (from 3 to 10)</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">...</th>
+                                <td>And more bonuses!</td>
+                                </tr>
+                            </tbody>
+                            </table>
+
+                            <hr/>
+                            <span className="text-danger">For now, payments are disabled!</span>
+                        </div>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" disabled>Buy now</Button>&nbsp;
+                    <Button variant="secondary" onClick={() => setVipModalOpened(false)}>Close</Button>
+                </Modal.Footer>
+            </Modal>
         </>}
     </>)
 }
